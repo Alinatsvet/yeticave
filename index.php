601d52b1ad07d2439963279288e4b8518a99baa4
Alinatsvet <koks1426@mail.ru>
2021-03-02 14:32:36 +0300

    3PRACTICHESKAI

--- index.php
+++ index.php
@@ -50,7 +50,26 @@ $data_list = [
 ],
 
 
-]
+];
+
+function sum_format($number)
+{
+    $number = ceil($number);
+    $f = true;
+    if ($number>=1000){
+        $price_n = number_format($number, 0, '.',' ');
+    }
+    else{
+        $price_n=$number;
+    }
+
+    if($f==true){
+        return $price_n. '<b class=\"rub\">p</b>';
+    }
+    else{
+        return $price_n;
+    }
+}
 
 ?>
 <!DOCTYPE html>
@@ -130,7 +149,7 @@ $data_list = [
                 <div class="lot__state">
                     <div class="lot__rate">
                         <span class="lot__amount">Стартовая цена</span>
-                        <span class="lot__cost"><?=$val['Price']?><b class="rub">р</b></span>
+                        <span class="lot__cost"><?=sum_format($val['Price'])?><b class="rub">р</b></span>
                     </div>
                     <div class="lot__timer timer">
                         12:23
